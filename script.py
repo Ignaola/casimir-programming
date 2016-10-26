8f56a6f9b79b2a0ae85e0508aef295c6b3047933
Ignacioolavarr <nacholavarria@gmail.com>
2016-10-26 09:27:05 +0000

    include a script that use test

--- script.py
+++ script.py
@@ -1,1 +1,7 @@
+import test
+
+
+print('the area of a circle of radius 3 is',test.circunference(3))
+print('the area of a circle of radius 5 is',test.areac(5))
+
 print('demonios la wea complicada')
