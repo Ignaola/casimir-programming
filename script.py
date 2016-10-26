6e3e82f3fe75f035c357a70bf95620355f4c7efb
tjcui <thijscui@gmail.com>
2016-10-26 11:46:33 +0000

    changed a comment

--- script.py
+++ script.py
@@ -3,9 +3,10 @@ import matplotlib.pyplot as plt
 
 
 print('the area of a circle of radius 3 is',test.circunference(3))
+
 print('the area of a circle of radius 5 is',test.areac(5))
 
-print('demonios la wea complicada')
+print('demonios la wea complicadaasdfasdfasd')
 
 
 
--- test.py
+++ test.py
@@ -6,6 +6,7 @@ print('blablalalalalala')
 import matplotlib.pyplot as plt
 
 circle1 = plt.Circle((0, 0), 0.2, color='r')
+fig.savefig('plotcircles.png')
 
 from math import pi
 import numpy as np
