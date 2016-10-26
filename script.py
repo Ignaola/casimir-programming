7c15713cc246461c969c8f62bfdafd3a48a57c01
tjcui <thijscui@gmail.com>
2016-10-26 11:49:57 +0000

    try again

--- script.py
+++ script.py
@@ -6,7 +6,7 @@ print('the area of a circle of radius 3 is',test.circunference(3))
 
 print('the area of a circle of radius 5 is',test.areac(5))
 
-print('demonios la wea complicadaasdfasdfasd')
+print('demonios la wea complicada')
 
 
 
