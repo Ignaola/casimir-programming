27c960ad64042f1ed8e886f51b740bb923882448
tjcui <thijscui@gmail.com>
2016-10-26 11:39:48 +0000

    added plot circle

--- test.py
+++ test.py
@@ -1,6 +1,12 @@
 print('hellow world and i like chicken') #here i add some useless comments
 
 
+print('blablalalalalala')
+
+import matplotlib.pyplot as plt
+
+circle1 = plt.Circle((0, 0), 0.2, color='r')
+
 from math import pi
 import numpy as np
 
@@ -9,5 +15,4 @@ def circunference(r):
     return 2*r*pi
 
 def areac(r):
-    print(pi*r**2)
     return pi*r**2
