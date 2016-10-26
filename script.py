d96541fdf78a4f65ec1f959cf948f262a485861e
Ignacioolavarr <nacholavarria@gmail.com>
2016-10-26 11:42:28 +0000

    myplot

--- script.py
+++ script.py
@@ -1,7 +1,14 @@
 import test
+import matplotlib.pyplot as plt
 
 
 print('the area of a circle of radius 3 is',test.circunference(3))
 print('the area of a circle of radius 5 is',test.areac(5))
 
 print('demonios la wea complicada')
+
+
+
+circle=plt.Circle((0,0),2)
+
+plt.show()
