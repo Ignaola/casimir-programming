0245dc34f408e986ab6e9756870f45607ac26524
Ignacioolavarr <nacholavarria@gmail.com>
2016-10-27 14:13:57 +0000

    first attempt

---
+++ peakfinder/peakfuntion.py
@@ -0,0 +1,17 @@
+import numpy as np
+#import matplotlib.pyplot as plt
+from math import pi, e
+import peakutils
+
+from scipy.signal import find_peaks_cwt
+
+cb = np.array([-0.010223, ... ])
+indexes = find_peaks_cwt(cb, np.arange(1, 550))
+
+def myfind_peak(xdata,ydata, min_height, min_dis):
+    """this functions should get the data as an x vector and a y vector
+    and return the vector with the indexes and the position in x (x[indexes]) of all the peaks in de signal y
+    """
+    indexes=peakutils.indexes(y,thres=min_height, min_dist=min_dis)
+    return indexes,x[indexes]
+    
